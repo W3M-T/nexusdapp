1a4b34f12dae5819b8874eb8f37154b0aead12ac
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2022-10-08 19:27:19 -0400

    update meta content

--- config/constants.ts
+++ config/constants.ts
@@ -5,9 +5,10 @@ export const dappHostname = process.env.NEXT_PUBLIC_DAPP_HOST;
 
 // HTML metata and og tags, default values for MetaHead.tsx component
 export const defaultMetaTags = {
-  title: "Elrond NextJS dapp demo - Elrond blockchain",
-  description: "Open source Dapp template for the Elrond blockchain.",
-  image: `${dappHostname}/og-image.png`,
+  title: "The NFTs Nexus porject",
+  description:
+    "The Nexus staking service. NFT creators will be alble to create pools for their collection so their community can stake their NFTs.",
+  image: `${dappHostname}/logo.png`,
 };
 
 // Only allow admin to go to admin pages
