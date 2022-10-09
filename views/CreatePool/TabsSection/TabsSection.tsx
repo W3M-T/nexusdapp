0e7180850b562eeaaf55d284bb85db4621dc6b80
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2022-10-09 10:00:20 -0400

    remove redirect in create pool

--- views/CreatePool/TabsSection/TabsSection.tsx
+++ views/CreatePool/TabsSection/TabsSection.tsx
@@ -13,7 +13,6 @@ import FormTab from "./FormTab/FormTab";
 import { useEffect } from "react";
 import { useAppDispatch } from "../../../shared/hooks/core/useRedux";
 import { setCreatePool } from "../../../shared/redux/slices/pools";
-import { store } from "../../../shared/redux/store";
 import TabsList from "./TabsList/TabsList";
 import UserTab from "./VerifyTab/VerifyTab";
 
@@ -48,43 +47,56 @@ const TabsSection = () => {
     }
   };
 
-  useEffect(() => {
-    const phase = localStorage.getItem("poolcreationPhase");
+  // useEffect(() => {
+  //   const phase = localStorage.getItem("poolcreationPhase");
 
-    if (phase === "2") {
-      setTimeout(() => {
-        activeFeeTab();
-      }, 500);
-    } else if (phase === "3") {
-      setTimeout(() => {
-        activeFormTab();
-      }, 500);
-    }
-  }, []);
+  //   if (phase === "2") {
+  //     setTimeout(() => {
+  //       activeFeeTab();
+  //     }, 500);
+  //   } else if (phase === "3") {
+  //     setTimeout(() => {
+  //       activeFormTab();
+  //     }, 500);
+  //   }
+  // }, []);
 
+  // useEffect(() => {
+  //   dispatch(
+  //     setCreatePool(
+  //       localStorage.getItem("createPool")
+  //         ? JSON.parse(localStorage.getItem("createPool"))
+  //         : {
+  //             collection: null,
+  //             phase1: null,
+  //             phase2: {
+  //               message: "",
+  //               status: "success",
+  //               data: null,
+  //             },
+  //           }
+  //     )
+  //   );
+  // }, [dispatch]);
   useEffect(() => {
     dispatch(
-      setCreatePool(
-        localStorage.getItem("createPool")
-          ? JSON.parse(localStorage.getItem("createPool"))
-          : {
-              collection: null,
-              phase1: null,
-              phase2: {
-                message: "",
-                status: "success",
-                data: null,
-              },
-            }
-      )
+      setCreatePool({
+        collection: null,
+        phase1: null,
+        phase2: {
+          message: "",
+          status: "success",
+          data: null,
+        },
+      })
     );
   }, [dispatch]);
-  useEffect(() => {
-    store.subscribe(() => {
-      const createPoolState = store.getState().pools.createPool;
-      localStorage.setItem("createPool", JSON.stringify(createPoolState));
-    });
-  }, []);
+  // useEffect(() => {
+  //   store.subscribe(() => {
+  //     const createPoolState = store.getState().pools.createPool;
+  //     localStorage.setItem("createPool", JSON.stringify(createPoolState));
+  //   });
+  // }, []);
 
   return (
     <Tabs
