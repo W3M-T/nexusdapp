d21811750447e5e1b70c48bb36226d2cab13694d
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2022-11-04 16:25:40 -0400

    If now > timestamp + 30 days for one staked NFT, then you must notify the user that he must unstake the nft from the pool. How? By making the border glow red and make o popup hover next to the mouse that says "Pool has reached its end. You must unstake your NFT."

--- views/Home/StakingSection/NFTCard/NFTCard.tsx
+++ views/Home/StakingSection/NFTCard/NFTCard.tsx
@@ -1,56 +1,83 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Tooltip } from "@chakra-ui/react";
+import { addDays } from "date-fns";
+import { useEffect, useState } from "react";
 import NextImg from "../../../../shared/components/ui/NextImg";
 import { IStakedWithTokenDetails } from "../../../../shared/redux/types/pools.interface";
 import { formatBalance } from "../../../../shared/utils/formatBalance";
 import { formatTokenI } from "../../../../shared/utils/formatTokenIdentifier";
+
 interface IProps {
   nft: IStakedWithTokenDetails;
   onClick: () => void;
 }
 const NFTCard = ({ nft, onClick }: IProps) => {
+  const [isNew, setIsNew] = useState(true);
+  useEffect(() => {
+    let isNew = true;
+    const p = nft.nftPool;
+    const date = new Date(p.timestam * 1000);
+
+    const dateInAMonth = addDays(date, 30);
+    const today = new Date();
+
+    if (dateInAMonth < today) {
+      isNew = false;
+    } else {
+      isNew = true;
+    }
+
+    setIsNew(isNew);
+  }, [nft]);
+
   return (
-    <Flex
-      height="full"
-      cursor="pointer"
-      position={"relative"}
-      flexDir="column"
-      onClick={onClick}
-      border="1px solid"
-      borderColor={"dappTemplate.color2.base"}
-      borderRadius="0.7rem"
-      padding={"4"}
-      bg="black"
+    <Tooltip
+      label="Pool has reached its end. You must unstake your NFT."
+      borderRadius={"5px"}
+      isDisabled={isNew}
     >
-      <Text fontSize={"xl"} fontWeight="bold" mb={1}>
-        {nft.name}
-      </Text>{" "}
-      <Flex mb={3} fontSize="small" flexDir={{ sm: "column", lsm: "row" }}>
-        <Text color="gray.400" fontWeight="bold" mr={1}>
-          Est. Rewards:
+      <Flex
+        height="full"
+        cursor="pointer"
+        position={"relative"}
+        flexDir="column"
+        onClick={onClick}
+        border="1px solid"
+        borderColor={isNew ? "dappTemplate.color2.base" : "red"}
+        borderRadius="0.7rem"
+        padding={"4"}
+        bg="black"
+      >
+        <Text fontSize={"xl"} fontWeight="bold" mb={1}>
+          {nft.name}
         </Text>{" "}
-        <Text>
-          {formatBalance({
-            balance: nft.estimatedRewards,
-            decimals: nft.tokenDetails?.decimals,
-          })}{" "}
-          {formatTokenI(nft.nftPool.token)}
-        </Text>{" "}
+        <Flex mb={3} fontSize="small" flexDir={{ sm: "column", lsm: "row" }}>
+          <Text color="gray.400" fontWeight="bold" mr={1}>
+            Est. Rewards:
+          </Text>{" "}
+          <Text>
+            {formatBalance({
+              balance: nft.estimatedRewards,
+              decimals: nft.tokenDetails?.decimals,
+            })}{" "}
+            {formatTokenI(nft.nftPool.token)}
+          </Text>{" "}
+        </Flex>
+        <NextImg
+          alt={"nft"}
+          width={["160px", "160px", "160px", "253px"]}
+          sx={{
+            span: {
+              borderRadius: "1rem",
+            },
+          }}
+          src={nft.url}
+          nextProps={{
+            height: 490,
+            width: 490,
+          }}
+        />
       </Flex>
-      <NextImg
-        alt={"nft"}
-        width={["160px", "160px", "160px", "253px"]}
-        sx={{
-          span: {
-            borderRadius: "1rem",
-          },
-        }}
-        src={nft.url}
-        nextProps={{
-          height: 490,
-          width: 490,
-        }}
-      />
-    </Flex>
+    </Tooltip>
   );
 };
 
