b46e924be9e145e6e0cd583df6ae40efe8847a84
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2022-11-04 16:31:44 -0400

    Only one thing, do task 4 also in the AEN pools

--- views/AenPoolView/AenPoolView.tsx
+++ views/AenPoolView/AenPoolView.tsx
@@ -1,13 +1,14 @@
 import { Flex, Heading } from "@chakra-ui/react";
 import { addDays } from "date-fns";
 import orderBy from "lodash/orderBy";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { MainLayout } from "../../shared/components/ui/MainLayout";
 import PoolItem from "../../shared/components/ui/PoolItem";
 import {
   useAppDispatch,
   useAppSelector,
 } from "../../shared/hooks/core/useRedux";
+import useGroupByField from "../../shared/hooks/tools/useGroupByField";
 import {
   fetchExistringPools,
   fetcHhasStakedForAEN,
@@ -15,12 +16,19 @@ import {
 import { fetchNfts } from "../../shared/redux/reduxAsyncFuncs/tokensFuncs";
 import { selectExistingPools } from "../../shared/redux/slices/pools";
 import { selectUserAddress } from "../../shared/redux/slices/settings";
+import { IExistingPool } from "../../shared/redux/types/pools.interface";
 import { route } from "../../shared/utils/routes";
 
 const AenPoolView = () => {
   const { data: pools } = useAppSelector(selectExistingPools);
   const dispatch = useAppDispatch();
   const connectedAddress = useAppSelector(selectUserAddress);
+
+  const [poolData, setPoolData] = useState([]);
+  const poolsGroupedByCollection: IExistingPool[][] = useGroupByField(
+    pools,
+    "collection"
+  );
 
   useEffect(() => {
     if (connectedAddress) {
@@ -29,6 +37,39 @@ const AenPoolView = () => {
       dispatch(fetchNfts(connectedAddress));
     }
   }, [dispatch, connectedAddress]);
+
+  useEffect(() => {
+    const newPoolsCollections = [...poolsGroupedByCollection];
+
+    const colelctionsPools = newPoolsCollections.flatMap((collectionPool) => {
+      const newPools = orderBy(
+        collectionPool,
+        [
+          function (pool) {
+            return Number(pool.nftsNow);
+          },
+        ],
+        "asc"
+      ).map((pool, i) => {
+        if (collectionPool.length > 1) {
+          if (i === 0) {
+            return pool;
+          } else {
+            const data: IExistingPool = {
+              ...pool,
+              poolName: pool.poolName + " " + (i + 1),
+            };
+            return data;
+          }
+        } else {
+          return pool;
+        }
+      });
+      return newPools;
+    });
+    setPoolData(colelctionsPools);
+  }, [poolsGroupedByCollection]);
+
   return (
     <MainLayout metaTitle={route.aenPools.name}>
       <Heading as={"h1"} w="full" textAlign={"center"} mt={10} mb={4}>
@@ -42,15 +83,7 @@ const AenPoolView = () => {
         flexWrap="wrap"
         mt={10}
       >
-        {orderBy(
-          pools,
-          [
-            function (pool) {
-              return Number(pool.nftsNow);
-            },
-          ],
-          "asc"
-        )
+        {poolData
           .filter((p) => {
             const date = new Date(p.timestam * 1000);
 
