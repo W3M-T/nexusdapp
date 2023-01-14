94923d71f19d9e21660ce5133497f80d89c86242
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-01-14 13:55:05 -0500

    transfer the MERMAID with the endpoint, when claiming

--- shared/services/sc/calls/multiTx/claimRewards.ts
+++ shared/services/sc/calls/multiTx/claimRewards.ts
@@ -18,55 +18,37 @@ export const claimUserRewards = async (
 
   const sender = store.getState().settings.userAddress;
   const senderAddress = new Address(sender);
-  nfts.forEach((nft) => {
-    const payload = TransactionPayload.contractCall()
-      .setFunction(new ContractFunction("claimRewards"))
-      .setArgs([
-        BytesValue.fromUTF8(nft.token),
-        new BigUIntValue(new BigNumber(nft.nonce)),
-      ])
-      .build();
 
-    const receiverAddress = new Address(
-      selectedNetwork.contractAddr.nftsStaking
-    );
-
-    const tx = new Transaction({
-      sender: senderAddress,
-      value: 0,
-      receiver: receiverAddress,
-      data: payload,
-      gasLimit: 70000000,
-      chainID: selectedNetwork.shortId,
-    });
-    transactions.push(tx);
-  });
-
+  const tokenIdentifier = selectedNetwork.tokens.MERMAID.identifier;
+  const multiplyier = Math.pow(10, selectedNetwork.tokens.MERMAID.decimals);
+  const finalValue = 5 * multiplyier;
   if (selectedNetwork.tokens?.MERMAID.identifier) {
-    const tokenIdentifier = selectedNetwork.tokens.MERMAID.identifier;
-    const multiplyier = Math.pow(10, selectedNetwork.tokens.MERMAID.decimals);
-    const finalValue = 5 * multiplyier;
-    const payload2 = TransactionPayload.contractCall()
-      .setFunction(new ContractFunction("ESDTTransfer"))
-      .setArgs([
-        BytesValue.fromUTF8(tokenIdentifier),
-        new BigUIntValue(new BigNumber(finalValue)),
-      ])
-      .build();
+    nfts.forEach((nft) => {
+      const payload = TransactionPayload.contractCall()
+        .setFunction(new ContractFunction("ESDTTransfer"))
+        .setArgs([
+          BytesValue.fromUTF8(tokenIdentifier),
+          new BigUIntValue(new BigNumber(finalValue)),
+          BytesValue.fromUTF8("claimRewards"),
+          BytesValue.fromUTF8(nft.token),
+          new BigUIntValue(new BigNumber(nft.nonce)),
+        ])
+        .build();
 
-    const tx = new Transaction({
-      sender: senderAddress,
-      receiver: new Address(
-        "erd10fq6af9vkr6usqc4wf9adsqhdvfz7d0d57pkag5ecmac7486zncsunge5m"
-      ),
-      value: 0,
-      data: payload2,
+      const receiverAddress = new Address(
+        selectedNetwork.contractAddr.nftsStaking
+      );
 
-      gasLimit: 7000000,
-      chainID: selectedNetwork.shortId,
+      const tx = new Transaction({
+        sender: senderAddress,
+        value: 0,
+        receiver: receiverAddress,
+        data: payload,
+        gasLimit: 70000000,
+        chainID: selectedNetwork.shortId,
+      });
+      transactions.push(tx);
     });
-
-    transactions.push(tx);
   }
 
   return await sendMultipleTransactions({ txs: transactions });
