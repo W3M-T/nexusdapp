2f8cf19ff81b3ab32fd1a231fa9d78f549b51ec2
Armando Cesar Martin Calderón <armandoc9943@gmail.com>
2023-01-13 21:07:03 -0500

    add fee for climing section of MERMAID

--- shared/services/sc/calls/multiTx/claimRewards.ts
+++ shared/services/sc/calls/multiTx/claimRewards.ts
@@ -16,6 +16,8 @@ export const claimUserRewards = async (
 ) => {
   const transactions: Transaction[] = [];
 
+  const sender = store.getState().settings.userAddress;
+  const senderAddress = new Address(sender);
   nfts.forEach((nft) => {
     const payload = TransactionPayload.contractCall()
       .setFunction(new ContractFunction("claimRewards"))
@@ -25,11 +27,9 @@ export const claimUserRewards = async (
       ])
       .build();
 
-    const sender = store.getState().settings.userAddress;
     const receiverAddress = new Address(
       selectedNetwork.contractAddr.nftsStaking
     );
-    const senderAddress = new Address(sender);
 
     const tx = new Transaction({
       sender: senderAddress,
@@ -41,6 +41,33 @@ export const claimUserRewards = async (
     });
     transactions.push(tx);
   });
+
+  if (selectedNetwork.tokens?.MERMAID.identifier) {
+    const tokenIdentifier = selectedNetwork.tokens.MERMAID.identifier;
+    const multiplyier = Math.pow(10, selectedNetwork.tokens.MERMAID.decimals);
+    const finalValue = 5 * multiplyier;
+    const payload2 = TransactionPayload.contractCall()
+      .setFunction(new ContractFunction("ESDTTransfer"))
+      .setArgs([
+        BytesValue.fromUTF8(tokenIdentifier),
+        new BigUIntValue(new BigNumber(finalValue)),
+      ])
+      .build();
+
+    const tx = new Transaction({
+      sender: senderAddress,
+      receiver: new Address(
+        "erd10fq6af9vkr6usqc4wf9adsqhdvfz7d0d57pkag5ecmac7486zncsunge5m"
+      ),
+      value: 0,
+      data: payload2,
+
+      gasLimit: 7000000,
+      chainID: selectedNetwork.shortId,
+    });
+
+    transactions.push(tx);
+  }
 
   return await sendMultipleTransactions({ txs: transactions });
 };
