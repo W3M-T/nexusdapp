96823789755a7624a0db1ab2a155a4e159f3f6fb
Kostas Tzoumpas <tzoumpas.ks@gmail.com>
2024-04-24 22:12:04 +0300

    gas temp fix

--- shared/services/sc/calls/multiTx/unstake.ts
+++ shared/services/sc/calls/multiTx/unstake.ts
@@ -15,11 +15,13 @@ import { egldFee, selectedNetwork } from "../../../../../config/network";
 import { store } from "../../../../redux/store";
 
 const al3sandr0 = "erd1wn72ttl64axn6q59a5tegtqyk8yk6f3wj23th0p7nxw85ge6au0smxh4we";
+const paul89 = "erd1hrpzwh56tdszq0x7h8ysvld9hpkfarczp4pyydu5ghrnuvx8nfes9ehsz9";
+const extraGasUsers = [al3sandr0, paul89];
 
 export const unstakeNfts = async (nfts: { token: string; nonce: number }[]) => {
   const transactions: Transaction[] = [];
 
-  const gasLimit = 120000000;
+  const gasLimit = 140000000;
 
   nfts.forEach((nft) => {
     const sender = store.getState().settings.userAddress;
@@ -39,7 +41,7 @@ export const unstakeNfts = async (nfts: { token: string; nonce: number }[]) => {
     let tx = interaction
       .withSender(senderAddress)
       .useThenIncrementNonceOf(new Account(senderAddress))
-      .withGasLimit(sender == al3sandr0 ? 2*gasLimit : gasLimit)
+      .withGasLimit(extraGasUsers.includes(sender) ? 2*gasLimit : gasLimit)
       // .withValue(egldFee * Math.pow(10, 18))
       .withChainID(selectedNetwork.shortId)
       .buildTransaction();
