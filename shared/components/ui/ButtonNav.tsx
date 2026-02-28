6ea1c196836dd4317991d9742e6b6be41a9e2c6e
Pensy Group <101661564+W3M-T@users.noreply.github.com>
2026-02-28 00:29:39 -0600

    add ALPHA badge to Nexus game nav item

--- shared/components/ui/ButtonNav.tsx
+++ shared/components/ui/ButtonNav.tsx
@@ -3,50 +3,73 @@ import { useGetLoginInfo } from "@multiversx/sdk-dapp/hooks/account/useGetLoginI
 import Link from "next/link";
 import { customColors } from "../../../config/chakraTheme";
 import { routes } from "../../utils/routes";
-const ButtonNav = () => {
-  const { isLoggedIn } = useGetLoginInfo();
 
-  return (
-    <Flex
-      w="full"
-      position={"fixed"}
-      bottom={0}
-      left={0}
-      right={0}
-      bg={customColors.myCustomColor.darker}
-      px={{ sm: 5, lsm: 7 }}
-      py={3}
-      justifyContent="space-around"
-      display={{ sm: "flex", md: "none" }}
-      zIndex={1000}
-    >
-      {routes.map((route) => {
-        if ((!isLoggedIn && !route.public) || route.outNav || route.hideFromMobile) {
-          // if (route.route !== "/") {
-          return null;
-          // }
-        }
-        return (
-          <Link href={route.route} key={route.route}>
-            <Flex
-              fontWeight={"bold"}
-              _hover={{
-                textShadow: "0 0 10px " + customColors.color2.base,
-              }}
-              flexDir="column"
-              justifyContent={"center"}
-              alignItems="center"
-            >
-              <Box>
-                <Icon as={route.icon} fontSize="25px" />
-              </Box>
-              <Text fontSize="10px">{route.name}</Text>
-            </Flex>
-          </Link>
+const ButtonNav = () => {
+    const { isLoggedIn } = useGetLoginInfo();
+
+    return (
+          <Flex
+                  w="full"
+                  position={"fixed"}
+                  bottom={0}
+                  left={0}
+                  right={0}
+                  bg={customColors.myCustomColor.darker}
+                  px={{ sm: 5, lsm: 7 }}
+                  py={3}
+                  justifyContent="space-around"
+                  display={{ sm: "flex", md: "none" }}
+                  zIndex={1000}
+                >
+            {routes.map((route) => {
+                          if ((!isLoggedIn && !route.public) || route.outNav || route.hideFromMobile) {
+                                      // if (route.route !== "/") {
+                                      return null;
+                                      // }
+                          }
+                          const isNexusGame = route.name === "Nexus";
+                          return (
+                                      <Link href={route.route} key={route.route}>
+                                                  <Flex
+                                                                  fontWeight={"bold"}
+                                                                  _hover={{
+                                                                                    textShadow: "0 0 10px " + customColors.color2.base,
+                                                                  }}
+                                                                  flexDir="column"
+                                                                  justifyContent={"center"}
+                                                                  alignItems="center"
+                                                                  position="relative"
+                                                                >
+                                                    {isNexusGame && (
+                                                                                  <Box
+                                                                                                      position="absolute"
+                                                                                                      top="-10px"
+                                                                                                      right="-12px"
+                                                                                                      bg="orange.400"
+                                                                                                      color="black"
+                                                                                                      fontSize="6px"
+                                                                                                      fontWeight="extrabold"
+                                                                                                      px="4px"
+                                                                                                      py="1px"
+                                                                                                      borderRadius="sm"
+                                                                                                      lineHeight="1.2"
+                                                                                                      letterSpacing="0.5px"
+                                                                                                      transform="rotate(12deg)"
+                                                                                                      zIndex={1}
+                                                                                                    >
+                                                                                                    ALPHA
+                                                                                  </Box>Box>
+                                                                )}
+                                                                <Box>
+                                                                                <Icon as={route.icon} fontSize="25px" />
+                                                                </Box>Box>
+                                                                <Text fontSize="10px">{route.name}</Text>Text>
+                                                  </Flex>Flex>
+                                      </Link>Link>
+                                    );
+                })}
+          </Flex>Flex>
         );
-      })}
-    </Flex>
-  );
 };
 
-export default ButtonNav;
+export default ButtonNav;</Flex>
