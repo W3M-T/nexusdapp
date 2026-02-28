7e9e75ce85db4f356efcd0cc2ba6e41c0bd414f0
Pensy Group <101661564+W3M-T@users.noreply.github.com>
2026-02-28 00:26:12 -0600

    replace Meme with Nexus game route

--- shared/utils/routes.ts
+++ shared/utils/routes.ts
@@ -1,7 +1,7 @@
 import {
-  AiOutlineHome,
-  AiOutlineUpCircle,
-  AiOutlineUser,
+    AiOutlineHome,
+    AiOutlineUpCircle,
+    AiOutlineUser,
 } from "react-icons/ai";
 // import { CgCloseR } from "react-icons/cg";
 // import { FaFaucet } from "react-icons/fa";
@@ -15,90 +15,91 @@ import { PiStackDuotone } from "react-icons/pi";
 import { PiWalletBold } from "react-icons/pi";
 import { LuBadgeHelp } from "react-icons/lu";
 import { TbBrandCashapp } from "react-icons/tb";
+import { GiCompass } from "react-icons/gi";
 
 interface IRoute {
-  route: string;
-  name: string;
-  outNav?: boolean;
-  public?: boolean;
-  icon?: IconType;
-  hideFromMobile?: boolean;
+    route: string;
+    name: string;
+    outNav?: boolean;
+    public?: boolean;
+    icon?: IconType;
+    hideFromMobile?: boolean;
 }
 
 export const route = {
-  home: {
-    route: "/home",
-    name: "Home",
-    public: true,
-    icon: AiOutlineHome,
-  },
-  nftmarketplace: {
-    route: "/nft-marketplace",
-    name: "NFT Marketplace",
-    public: true,
-    icon: RiNftFill,
-    hideFromMobile: false,
-  },
-  staked: {
-    route: "/staked",
-    name: "Staked NFTs",
-    public: true,
-    outNav: true,
-    icon: RiNftFill,
-    hideFromMobile: true,
-  },
-  create: {
-    route: "/create-pool",
-    name: "Create Pool",
-    public: false,
-    outNav: true,
-    icon: TbPyramid,
-  },
-  view: {
-    route: "/staking-pools",
-    name: "Pools",
-    public: true,
-    icon: PiStackDuotone,
-    hideFromMobile: true,
-  },
-  faucets: {
-    route: "/faucets",
-    name: "Faucets",
-    public: true,
-    icon: GiReceiveMoney,
-  },
-  support: {
-    route: "https://mvxblob.com",
-    name: "Meme",
-    public: true,
-    icon: TbBrandCashapp,
-  },
-  scOwner: {
-    route: "/sc-owner-dashboard",
-    name: "SC Owner",
-    outNav: true,
-    icon: AiOutlineUser,
-  },
-  nftCollectionOwner: {
-    route: "/nft-collection-owner",
-    name: "Nft Collection Owner",
-    outNav: true,
-    icon: AiOutlineUpCircle,
-  },
+    home: {
+          route: "/home",
+          name: "Home",
+          public: true,
+          icon: AiOutlineHome,
+    },
+    nftmarketplace: {
+          route: "/nft-marketplace",
+          name: "NFT Marketplace",
+          public: true,
+          icon: RiNftFill,
+          hideFromMobile: false,
+    },
+    staked: {
+          route: "/staked",
+          name: "Staked NFTs",
+          public: true,
+          outNav: true,
+          icon: RiNftFill,
+          hideFromMobile: true,
+    },
+    create: {
+          route: "/create-pool",
+          name: "Create Pool",
+          public: false,
+          outNav: true,
+          icon: TbPyramid,
+    },
+    view: {
+          route: "/staking-pools",
+          name: "Pools",
+          public: true,
+          icon: PiStackDuotone,
+          hideFromMobile: true,
+    },
+    faucets: {
+          route: "/faucets",
+          name: "Faucets",
+          public: true,
+          icon: GiReceiveMoney,
+    },
+    nexusGame: {
+          route: "/nexus-game",
+          name: "Nexus",
+          public: true,
+          icon: GiCompass,
+    },
+    scOwner: {
+          route: "/sc-owner-dashboard",
+          name: "SC Owner",
+          outNav: true,
+          icon: AiOutlineUser,
+    },
+    nftCollectionOwner: {
+          route: "/nft-collection-owner",
+          name: "Nft Collection Owner",
+          outNav: true,
+      icon: AiOutlineUpCircle,
+    },
 };
 
 export const actualHomepage = route.home;
 
 const routesFn = (): IRoute[] => {
-  const routes: IRoute[] = [];
-  for (const key in route) {
-    if (Object.prototype.hasOwnProperty.call(route, key)) {
-      const routeObj: IRoute = route[key];
-      routes.push(routeObj);
-    }
-  }
+    const routes: IRoute[] = [];
+        for (const key in route) {
+              if (Object.prototype.hasOwnProperty.call(route, key)) {
+                      const routeObj: IRoute = route[key];
+                      routes.push(routeObj);
+              }
+        }
 
-  return routes;
+    return routes;
 };
 
-export const routes = routesFn();
+      export const routes = routesFn();
