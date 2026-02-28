4c2b4f9ae6eef0cd9f877446e22f00202bbe3038
Pensy Group <101661564+W3M-T@users.noreply.github.com>
2026-02-28 00:32:59 -0600

    add ALPHA badge to desktop NavMenu for Nexus

--- shared/components/ui/NavMenu.tsx
+++ shared/components/ui/NavMenu.tsx
@@ -6,40 +6,63 @@ import { routes } from "../../utils/routes";
 import { useRouter } from "next/router";
 
 const NavMenu = () => {
-  const { isLoggedIn } = useGetLoginInfo();
-  const router = useRouter();
-  
-  return (
-    <Flex
-      alignItems={"center"}
-      gap={7}
-      mr={{ sm: 0, lg: "12" }}
-      display={{ sm: "none", md: "flex" }}
-    >
-      {routes.map((route) => {
-        if ((!isLoggedIn && !route.public) || route.outNav) {
-          // if (route.route !== "/") {
-          return null;
-          // }
-        }
-        const isActive = router.pathname === route.route;
-        return (
-          <Link href={route.route} key={route.route}>
-            <Box
-              fontSize={"md"}
-              fontWeight={"bold"}
-              textShadow={isActive ? "0 0 15px " + customColors.color2.base : "none"}
-              _hover={{
-                textShadow: "0 0 15px " + customColors.color2.base,
-              }}
-            >
-              {route.name}
-            </Box>
-          </Link>
+    const { isLoggedIn } = useGetLoginInfo();
+    const router = useRouter();
+
+    return (
+          <Flex
+                  alignItems={"center"}
+                  gap={7}
+                  mr={{ sm: 0, lg: "12" }}
+                  display={{ sm: "none", md: "flex" }}
+                >
+            {routes.map((route) => {
+                          if ((!isLoggedIn && !route.public) || route.outNav) {
+                                      // if (route.route !== "/") {
+                                      return null;
+                                      // }
+                          }
+                          const isActive = router.pathname === route.route;
+                          const isNexusGame = route.name === "Nexus";
+                          return (
+                                      <Link href={route.route} key={route.route}>
+                                                  <Box
+                                                                  fontSize={"md"}
+                                                                  fontWeight={"bold"}
+                                                                  textShadow={isActive ? "0 0 15px " + customColors.color2.base : "none"}
+                                                                  _hover={{
+                                                                                    textShadow: "0 0 15px " + customColors.color2.base,
+                                                                  }}
+                                                                  position="relative"
+                                                                  display="inline-block"
+                                                                >
+                                                    {route.name}
+                                                    {isNexusGame && (
+                                                                                  <Box
+                                                                                                      as="span"
+                                                                                                      position="absolute"
+                                                                                                      top="-8px"
+                                                                                                      right="-22px"
+                                                                                                      bg="orange.400"
+                                                                                                      color="black"
+                                                                                                      fontSize="7px"
+                                                                                                      fontWeight="extrabold"
+                                                                                                      px="3px"
+                                                                                                      py="1px"
+                                                                                                      borderRadius="sm"
+                                                                                                      lineHeight="1.2"
+                                                                                                      letterSpacing="0.5px"
+                                                                                                      transform="rotate(12deg)"
+                                                                                                    >
+                                                                                                    ALPHA
+                                                                                  </Box>Box>
+                                                                )}
+                                                  </Box>Box>
+                                      </Link>Link>
+                                    );
+                })}
+          </Flex>Flex>
         );
-      })}
-    </Flex>
-  );
 };
 
-export default NavMenu;
+export default NavMenu;</Flex>
